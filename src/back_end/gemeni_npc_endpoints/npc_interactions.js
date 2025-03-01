4c18aa3d7ba9297d760df25f3bf086f7b37f6005
Jonathan Cino <cino.jonathan@gmail.com>
2025-03-01 14:46:19 -0600

    added endpoints

--- src/back_end/gemeni_npc_endpoints/npc_interactions.js
+++ src/back_end/gemeni_npc_endpoints/npc_interactions.js
@@ -15,6 +15,10 @@ class npcrouter {
         this.language = language;
         this.first_flag = true;
         this.conversation_history_villager = []
+        this.conversation_history_innkeeper = []
+        this.conversation_history_shopkeeper = []
+        this.conversation_history_farmer = []
+        this.conversation_history_blacksmith = []
         this.initial_instruction = "Please do not respond with the prefix 'ai:'. Just provide the content of your response."
     }
 
@@ -26,7 +30,7 @@ async setUpRoutes() {
 this.router.get('/villager', async (req, res) => {
     const { prompt } = req.query
     if(prompt == "init") {
-        const init_prompt = "From now on no matter what you are acting as a poor villager in a pixel game where the user is interacting with you to learn a language. give a hello how are you in  " + this.language + " no emojis, and only a text response. Also never reference the fact that you are AI ever." 
+        const init_prompt = "From now on no matter what you are acting as a poor villager in a pixel game where the user is interacting with you to learn a language. But you cannot move or interact with them give a hello how are you in  " + this.language + " no emojis, and only a text response. Also never reference the fact that you are AI ever." 
         const response = await this.send_to_gemini(init_prompt, this.conversation_history_villager)
         res.status(200).send(response)
     }
@@ -36,13 +40,76 @@ this.router.get('/villager', async (req, res) => {
         res.status(200).send(response) 
     }
 }); 
+
+
+// Innkeeper endpoint 
+this.router.get('/innkeeper', async (req, res) => {
+    const { prompt } = req.query
+    if(prompt == "init") {
+        const init_prompt = "From now on no matter what you are acting as a poor innkeeper in a pixel game where the user is interacting with you to learn a language. But you cannot move or interact with them. give a hello how are you in  " + this.language + " no emojis, and only a text response. Also never reference the fact that you are AI ever." 
+        const response = await this.send_to_gemini(init_prompt, this.conversation_history_innkeeper)
+        res.status(200).send(response)
+    }
+    else { 
+        let conversation_prompt = "Rate the following user's " + this.language + " on a scale of 1 to 5. Place your rating as an integer value at the front of your response followed by a space, then give your 1 sentence response, as if you were a " + this.language + " innkeeper, and someone said this to you. Reply only in" + this.language + " USER INPUT: " + prompt;  
+        const response = await this.send_to_gemini(conversation_prompt, this.conversation_history_innkeeper); 
+        res.status(200).send(response) 
+    }
+}); 
+
+
+// Shopkeeper endpoint 
+this.router.get('/shopkeeper', async (req, res) => {
+    const { prompt } = req.query
+    if(prompt == "init") {
+        const init_prompt = "From now on no matter what you are acting as a poor shopkeeper in a pixel game where the user is interacting with you to learn a language. But you cannot move or interact with them. give a hello how are you in  " + this.language + " no emojis, and only a text response. Also never reference the fact that you are AI ever." 
+        const response = await this.send_to_gemini(init_prompt, this.conversation_history_shopkeeper)
+        res.status(200).send(response)
+    }
+    else { 
+        let conversation_prompt = "Rate the following user's " + this.language + " on a scale of 1 to 5. Place your rating as an integer value at the front of your response followed by a space, then give your 1 sentence response, as if you were a " + this.language + " shopkeeper, and someone said this to you. Reply only in" + this.language + " USER INPUT: " + prompt;  
+        const response = await this.send_to_gemini(conversation_prompt, this.conversation_history_shopkeeper); 
+        res.status(200).send(response) 
+    }
+}); 
+
+
+//Farmer endpoint 
+this.router.get('/farmer', async (req, res) => {
+    const { prompt } = req.query
+    if(prompt == "init") {
+        const init_prompt = "From now on no matter what you are acting as a poor farmer in a pixel game where the user is interacting with you to learn a language. But you cannot move or interact with them. give a hello how are you in  " + this.language + " no emojis, and only a text response. Also never reference the fact that you are AI ever." 
+        const response = await this.send_to_gemini(init_prompt, this.conversation_history_farmer)
+        res.status(200).send(response)
+    }
+    else { 
+        let conversation_prompt = "Rate the following user's " + this.language + " on a scale of 1 to 5. Place your rating as an integer value at the front of your response followed by a space, then give your 1 sentence response, as if you were a " + this.language + " Farmer, and someone said this to you. Reply only in" + this.language + " USER INPUT: " + prompt;  
+        const response = await this.send_to_gemini(conversation_prompt, this.conversation_history_farmer); 
+        res.status(200).send(response) 
+    }
+}); 
+
+
+// blacksmith endpoint 
+this.router.get('/blacksmith', async (req, res) => {
+    const { prompt } = req.query
+    if(prompt == "init") {
+        const init_prompt = "From now on no matter what you are acting as a poor blacksmith in a pixel game where the user is interacting with you to learn a language. But you cannot move or interact with them. give a hello how are you in  " + this.language + " no emojis, and only a text response. Also never reference the fact that you are AI ever." 
+        const response = await this.send_to_gemini(init_prompt, this.conversation_history_blacksmith)
+        res.status(200).send(response)
+    }
+    else { 
+        let conversation_prompt = "Rate the following user's " + this.language + " on a scale of 1 to 5. Place your rating as an integer value at the front of your response followed by a space, then give your 1 sentence response, as if you were a " + this.language + " Blacksmith, and someone said this to you. Reply only in" + this.language + " USER INPUT: " + prompt;  
+        const response = await this.send_to_gemini(conversation_prompt, this.conversation_history_blacksmith); 
+        res.status(200).send(response) 
+    }
+}); 
 }
 
 updateLanguage(newLanguage) {
     this.language = newLanguage;
     this.setUpRoutes();  // Re-setup routes with new language
   }
-
 
 
     async send_to_gemini(prompt, conversation_history) {
