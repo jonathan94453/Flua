9b481473bcdef78a3192e56ffa750ea0f44df01e
jackcase04 <jackcase04@gmail.com>
2025-02-28 21:41:43 -0600

    added basic map traversal

--- src/front_end/main.js
+++ src/front_end/main.js
@@ -1,24 +1,19 @@
+import { Start } from '../front_end/scenes/Start.js';
+
 const config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
-};
-
-const game = new Phaser.Game(config);
-
-function preload() {
-    this.load.image('logo', 'assets/phaser3-logo.png'); // Example asset
+    title: 'Flua',
+    parent: 'game-container',
+    width: 1280,
+    height: 720,
+    pixelArt: false,
+    scene: [
+        Start
+    ],
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
 }
-
-function create() {
-   this.add.text(300, 250, 'Hello, World!', { fontSize: '32px', fill: '#fff' });
-}
-
-function update() {
-    // Game loop logic
-}
+new Phaser.Game(config);
+            
---
+++ src/front_end/scenes/Start.js
@@ -0,0 +1,51 @@
+export class Start extends Phaser.Scene
+{
+    constructor()
+    {
+        super('Start');
+    }
+
+    preload()
+    {
+        this.load.image('background', '../../../../assets/GrassTexture.jpg');
+        this.load.image('logo', '../../../../assets/phaser.png');
+        this.load.image('player', '../../../../assets/WhiteCircle.png');
+
+        //  The ship sprite is CC0 from https://ansimuz.itch.io - check out his other work!
+        this.load.spritesheet('ship', '../../../../assets/spaceship.png', { frameWidth: 176, frameHeight: 96 });
+    }
+
+    create()
+    {
+        this.background = this.add.tileSprite(640, 360, 1280, 720, 'background');
+
+        const logo = this.add.image(640, 200, 'logo');
+
+        const player = this.add.sprite(640, 360, 'player');
+        player.setDisplaySize(50, 50);
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+    }
+
+    update()
+    {
+        if (this.cursors.right.isDown) {
+            this.background.tilePositionX += 2;
+        }
+
+        if (this.cursors.left.isDown) {
+            this.background.tilePositionX -= 2;
+        }
+
+        if (this.cursors.down.isDown) {
+            this.background.tilePositionY += 2;
+        }
+
+        if (this.cursors.up.isDown) {
+            this.background.tilePositionY -= 2;
+        }
+        
+    }
+}
+
